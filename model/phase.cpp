80f061c3b138dbd57c16045ddfba439d30ba68fa
neodc <noe@lal.be>
2014-12-20 08:03:31 +0100

    Add more phase.

--- model/phase.cpp
+++ model/phase.cpp
@@ -1,16 +1,49 @@
 #include "phase.h"
 
 std::ostream & operator<< ( std::ostream & out, const Phase& in){
+
 	switch (in){
-	case Phase::NO_PLAYER:
-		out << "no player";
-		break;
-	case Phase::PLAYING:
-		out << "playing";
-		break;
-	case Phase::END:
-		out << "end";
-		break;
+		case Phase::NO_PLAYER:
+			out << "no player";
+			break;
+		case Phase::PLAYING_DICE:
+			out << "playing dice";
+			break;
+		case Phase::PLAYING_EFFECT:
+			out << "playing effect";
+			break;
+		case Phase::PLAYING_MOVE:
+			out << "playing move";
+			break;
+		case Phase::PLAYING_ATTACK:
+			out << "playing attack";
+			break;
+		case Phase::END:
+			out << "end";
+			break;
 	}
 	return out;
+}
+
+Phase next(Phase phase){
+	switch (phase){
+		case Phase::NO_PLAYER:
+			return Phase::PLAYING_DICE;
+			break;
+		case Phase::PLAYING_DICE:
+			return Phase::PLAYING_EFFECT;
+			break;
+		case Phase::PLAYING_EFFECT:
+			return Phase::PLAYING_MOVE;
+			break;
+		case Phase::PLAYING_MOVE:
+			return Phase::PLAYING_ATTACK;
+			break;
+		case Phase::PLAYING_ATTACK:
+			return Phase::PLAYING_DICE;
+			break;
+		default:
+			return Phase::END;
+			break;
+	}
 }
--- model/phase.h
+++ model/phase.h
@@ -4,10 +4,15 @@
 
 enum class Phase : char{
 	NO_PLAYER,
-	PLAYING,
+	PLAYING_DICE,
+	PLAYING_EFFECT,
+	PLAYING_MOVE,
+	PLAYING_ATTACK,
 	END
 };
 
 std::ostream & operator<< (std::ostream & out, const Phase& in);
+
+Phase next(Phase phase);
 
 #endif // PHASE_H
