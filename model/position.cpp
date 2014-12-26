8436beaef95b60d6463b1b2471d76e7cb26db100
neodc <noe@lal.be>
2014-12-26 04:43:15 +0100

    Add str <=> pos convertion.

--- model/position.cpp
+++ model/position.cpp
@@ -1,8 +1,25 @@
 #include "position.h"
 
+#include <QStringList>
+
 std::ostream & operator<< ( std::ostream & out, const Position& in){
 	out << '{' << in.x << ',' << in.y << '}';
 	return out;
+}
+
+QString pos2str(const Position& pos){
+	return QString("%1:%2").arg(pos.x).arg(pos.y);
+}
+
+Position str2pos(const QString& s){
+	Position ret;
+
+	QStringList list{s.split(':')};
+
+	ret.x = list[0].toUInt();
+	ret.y = list[1].toUInt();
+
+	return ret;
 }
 
 bool operator<(const Position & first, const Position & second){
--- model/position.h
+++ model/position.h
@@ -1,6 +1,7 @@
 #ifndef POSITION_H
 #define POSITION_H
 #include <iostream>
+#include <QString>
 
 typedef struct{
 	unsigned x;
@@ -8,6 +9,9 @@ typedef struct{
 } Position;
 
 std::ostream & operator<< (std::ostream & out, const Position& in);
+
+QString pos2str(const Position& pos);
+Position str2pos(const QString& s);
 
 bool operator<(const Position & first, const Position & second);
 
